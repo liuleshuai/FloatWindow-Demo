55f912059c2f6bcf990ba43c75bbb7afaf58096f
liukuo <lk54321@yeah.net>
2018-05-14 11:05:01 +0800

    添加heads-up

--- app/src/main/java/com/example/liukuo/myapplication/WindowActivity.java
+++ app/src/main/java/com/example/liukuo/myapplication/WindowActivity.java
@@ -1,12 +1,18 @@
 package com.example.liukuo.myapplication;
 
 import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class WindowActivity extends AppCompatActivity {
@@ -15,7 +21,6 @@ public class WindowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        requestPermission();
     }
 
     private void requestPermission() {
@@ -42,5 +47,34 @@ public class WindowActivity extends AppCompatActivity {
                 WindowUtils.showPopupWindow(this);
             }
         }
+    }
+
+    public void windowButton(View view) {
+        requestPermission();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void notifyButton(View view) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(2);
+        Notification.Builder builder = new Notification.Builder(this);
+        Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mIntent, 0);
+        builder.setContentIntent(pendingIntent);
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setAutoCancel(true);
+        builder.setContentTitle("悬挂式通知");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //设置点击跳转
+            Intent hangIntent = new Intent();
+            hangIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            hangIntent.setClass(this, WindowActivity.class);
+            //如果描述的PendingIntent已经存在，则在产生新的Intent之前会先取消掉当前的
+            PendingIntent hangPendingIntent = PendingIntent.getActivity(this, 0, hangIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            builder.setFullScreenIntent(hangPendingIntent, true);
+        }
+        notificationManager.notify(2, builder.build());
     }
 }
